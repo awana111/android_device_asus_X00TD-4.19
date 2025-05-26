dea4418bdbf86b33ea269c28aebc0c642c7bf281
awana <80144987+awana111@users.noreply.github.com>
2025-05-26 17:21:59 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,6 +12,7 @@ git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_s
 rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
 
 #Sign-Key
+rm -rf vendor/voltage-priv/keys
 croot && git clone https://github.com/VoltageOS/vendor_voltage-priv_keys vendor/voltage-priv/keys
 cd vendor/voltage-priv/keys
 ./keys.sh
