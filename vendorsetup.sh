f45bd0fe728008e77389e02ea3a93a419902dc84
awana <80144987+awana111@users.noreply.github.com>
2025-05-26 16:25:54 +0700

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,18 @@
+#clone device common tree
+rm -rf device/asus/sdm660-common
+git clone --depth=1 https://github.com/ikwfahmi/android_device_asus_sdm660-common-4.19 -b 15.3 device/asus/sdm660-common
+
+# Clone vendor tree
+rm -rf vendor/asus
+git clone --depth=1 https://github.com/ikwfahmi/proprietary_vendor_asus-4.19 vendor/asus
+
+# Clone prebuilt kernel
+rm -rf kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm660 -b stable kernel/asus/sdm660
+rm -rf kernel/asus/sdm660/KernelSU-Next/userspace
+
+#Sign-Key
+croot && git clone https://github.com/VoltageOS/vendor_voltage-priv_keys vendor/voltage-priv/keys
+cd vendor/voltage-priv/keys
+./keys.sh
+cd ../../..
