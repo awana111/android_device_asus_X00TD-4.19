3977d15a61a4f0ecda8febbf36b3e6e5e7fab8b9
awana <80144987+awana111@users.noreply.github.com>
2025-06-01 10:47:26 +0700

    X00TD : Bringup PixelOS

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -10,14 +10,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common crDroid stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
 
-WITH_GMS := false
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit some common device props
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -27,7 +28,7 @@ TARGET_EXCLUDES_AUDIOFX := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := aosp_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
