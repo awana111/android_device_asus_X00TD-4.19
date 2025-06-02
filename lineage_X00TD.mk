0494768922275e6008ff7cb258d7f7a3d8f2e9f8
awana <80144987+awana111@users.noreply.github.com>
2025-06-02 08:52:20 +0700

    Update lineage_X00TD.mk

--- lineage_X00TD.mk
+++ lineage_X00TD.mk
@@ -9,15 +9,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common crDroid stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DroidX-UI stuff
+$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 TARGET_BUILD_DEVICE_AS_WEBCAM := true
 
-WITH_GMS := false
+# DroidX-UI stuff
+DROIDX_BUILD_TYPE := UNOFFICIAL
+DROIDX_GAPPS := true
 
 # Inherit some common device props
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -27,7 +29,7 @@ TARGET_EXCLUDES_AUDIOFX := true
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := droidx_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
